46aae609d9e26aee184b33ab6ae3cf37ad219ba1
Lisa Schneider <lisa.schneider@deltares.nl>
2022-02-24 13:02:52 +0100

    Second correction in setup.py of README.md

--- setup.py
+++ setup.py
@@ -1,34 +1,16 @@
-from setuptools import setup
-import os
-import sys
-
-_here = os.path.abspath(os.path.dirname(__file__))
-
-if sys.version_info[0] < 3:
-    with open(os.path.join(_here, 'README.md')) as f:
-        long_description = f.read()
-else:
-    with open(os.path.join(_here, 'README.md'), encoding='utf-8') as f:
-        long_description = f.read()
-
-version = {}
-with open(os.path.join(_here, 'somepackage', 'version.py')) as f:
-    exec(f.read(), version)
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
 
 setup(
-    name='WQ_tools',
-    version=version['__version__'],
-    description=('Scripts, functions and tools to deal with the water quality part of dfm..'),
-    long_description=long_description,
-    author='Lisa Schneider',
-    author_email='lisa.schneider@deltares.nl',
-    license='MIT',
-    packages=['WQ_tools'],
-    install_requires=['matplotlib>=3.0','datetime>=1.8.1'],
-    include_package_data=True,
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6'],
-    )
+    name="WQ_tools",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    zip_safe=False,
+    author="Lisa Schneider",
+    author_email="lisa.schneider@deltares.nl",
+    description=(
+        "This package provides the tools for the WQ part fo DFM"
+    ),
+    license="MIT",
+)
